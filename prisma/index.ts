ce95b2c7e355dfdefab2cfcbe860c5b0ece0d9eb
mxvsh <hi@monawwar.io>
2024-02-17 20:32:37 +0530

    fix: better prisma client

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,5 +1,15 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;

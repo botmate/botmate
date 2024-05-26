47ec5a2883595ac5601cfa24a62661e611eb524f
mxvsh <hi@monawwar.io>
2024-05-26 19:11:33 +0530

    chore: generate index html

--- packages/core/cli/src/cmds/build.ts
+++ packages/core/cli/src/cmds/build.ts
@@ -77,6 +77,25 @@ export function build(cmd: Command) {
 
       app.logger.debug('Building client...');
 
+      writeFile(
+        'index.html',
+        `<!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="utf-8" />
+          <title>BotMate</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+          <script type="module" src="/tmp/main.tsx"></script>
+        </head>
+        <body>
+          <div id="root"></div>
+        </body>
+      </html>
+      `,
+        'utf-8',
+      );
+
       await viteBuild({
         plugins: [
           react(),
@@ -99,5 +118,6 @@ export function build(cmd: Command) {
 
       await unlink('tmp/main.tsx');
       await unlink('tmp/plugins.ts');
+      await unlink('index.html');
     });
 }

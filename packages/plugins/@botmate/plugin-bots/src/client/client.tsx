8a98df7b7a69fce524410aa88e02b69ed6fa8797
mxvsh <hi@monawwar.io>
2024-05-30 21:23:45 +0530

    chore: add bots plugin

---
+++ packages/plugins/@botmate/plugin-bots/src/client/client.tsx
@@ -0,0 +1,9 @@
+import { Plugin } from '@botmate/client';
+
+export class BotsPlugin extends Plugin {
+  priority = 1;
+
+  async beforeLoad() {
+    //
+  }
+}
---
+++ packages/plugins/@botmate/plugin-bots/src/server/server.ts
@@ -0,0 +1,3 @@
+import { Plugin } from '@botmate/server';
+
+export class BotsPlugin extends Plugin {}

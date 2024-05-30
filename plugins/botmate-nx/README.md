a2ef0d10f020baf28c3b2542bb31e8b4b565c8be
mxvsh <hi@monawwar.io>
2024-05-30 21:23:16 +0530

    feat: add plugin generator

---
+++ plugins/botmate-nx/README.md
@@ -0,0 +1,11 @@
+# botmate-nx
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build botmate-nx` to build the library.
+
+## Running unit tests
+
+Run `nx test botmate-nx` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ plugins/botmate-nx/src/generators/plugin/generator.ts
@@ -0,0 +1,50 @@
+import {
+  ProjectConfiguration,
+  Tree,
+  formatFiles,
+  generateFiles,
+} from '@nx/devkit';
+import { libraryGenerator } from '@nx/node';
+import { camelCase, capitalize } from 'lodash/fp';
+
+import { PluginGeneratorSchema } from './schema';
+
+export async function pluginGenerator(
+  tree: Tree,
+  options: PluginGeneratorSchema,
+) {
+  const projectRoot = `packages/plugins/@botmate`;
+
+  options.name = `plugin-${options.name}`;
+
+  await libraryGenerator(tree, {
+    name: options.name,
+    directory: `${projectRoot}/${options.name}`,
+    projectNameAndRootFormat: 'as-provided',
+    publishable: true,
+    buildable: true,
+    importPath: `@botmate/${options.name}`,
+    compiler: 'tsc',
+  });
+
+  const projectJson = JSON.parse(
+    tree.read(`${projectRoot}/${options.name}/project.json`).toString(),
+  ) as ProjectConfiguration;
+  projectJson.sourceRoot = projectJson.sourceRoot.replace('/src', '');
+  projectJson.targets.build.options.rootDir = `${projectRoot}/${options.name}/src`;
+  tree.write(
+    `${projectRoot}/${options.name}/project.json`,
+    JSON.stringify(projectJson, null, 2),
+  );
+
+  const className =
+    capitalize(camelCase(options.name.replace('plugin-', ''))) + 'Plugin';
+
+  generateFiles(tree, `${__dirname}/files`, `${projectRoot}/${options.name}`, {
+    className,
+  });
+
+  await formatFiles(tree);
+}
+
+export default pluginGenerator;

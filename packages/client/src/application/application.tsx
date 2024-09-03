f932c9f4791ee20cfe33ba3d7f0a891df9af1785
mxvsh <hi@monawwar.io>
2024-09-03 22:40:22 +0530

    chore: setup sentry

--- packages/client/src/application/application.tsx
+++ packages/client/src/application/application.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/react';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider as ReduxProvider } from 'react-redux';
@@ -5,7 +6,6 @@ import { BrowserRouter, Route, RouteObject, Routes } from 'react-router-dom';
 
 import '@botmate/ui/styles.css';
 
-import MainLayout from './layouts/main';
 import SettingsLayout from './layouts/settings';
 import AnalyticsPage from './pages/bots/analytics';
 import DashboardPage from './pages/bots/dashboard';
@@ -57,6 +57,21 @@ export class Application {
   }
 
   render(container: string) {
+    Sentry.init({
+      dsn: 'https://5177382ad836b2ffc883c3938f310dfe@o4507889496096768.ingest.us.sentry.io/4507889504550912',
+      integrations: [
+        Sentry.browserTracingIntegration(),
+        Sentry.replayIntegration()
+      ],
+      // Tracing
+      tracesSampleRate: 1.0, //  Capture 100% of the transactions
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ['localhost'],
+      // Session Replay
+      replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+      replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+    });
+
     const element = document.getElementById(container);
     if (element) {
       const root = createRoot(element);

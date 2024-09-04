341c085a164ec0d101f08724b0b9a0707a5434b3
mxvsh <hi@monawwar.io>
2024-09-05 00:17:27 +0530

    style: add ui components

---
+++ packages/ui/src/components/ui/badge.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+
+import { type VariantProps, cva } from 'class-variance-authority';
+
+import { cn } from '../../utils';
+
+const badgeVariants = cva(
+  'inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 select-none',
+  {
+    variants: {
+      variant: {
+        default:
+          'border-transparent bg-primary text-primary-foreground hover:bg-primary/80',
+        warning:
+          'border-transparent bg-warning text-warning-foreground border border-warning-foreground/10',
+        danger:
+          'border-transparent bg-danger text-danger-foreground dark:border border-danger-foreground/10',
+        success:
+          'border-transparent bg-success text-success-foreground border border-success-foreground/10',
+        outline: 'text-foreground',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  },
+);
+
+export interface BadgeProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof badgeVariants> {}
+
+function Badge({ className, variant, ...props }: BadgeProps) {
+  return (
+    <div className={cn(badgeVariants({ variant }), className)} {...props} />
+  );
+}
+
+export { Badge, badgeVariants };
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -7,3 +7,5 @@ export * from './components/ui/checkbox';
 export * from './components/ui/tooltip';
 export * from './components/ui/hover-card';
 export * from './components/ui/dropdown-menu';
+export * from './components/ui/badge';
+export * from './components/ui/tabs';

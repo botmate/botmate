f94ece614c1ce1ef6059fc7b63c71a60bc7da9fd
mxvsh <hi@monawwar.io>
2024-09-05 00:13:08 +0530

    chore: add Api class

--- packages/client/src/application/api.ts
+++ packages/client/src/application/api.ts
@@ -4,3 +4,21 @@ export const baseApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: '/api' }),
   endpoints: () => ({}),
 });
+
+export class Api {
+  async get<TResponse = any>(url: string) {
+    const res = await fetch(`/api${url}`);
+    return (await res.json()) as TResponse;
+  }
+
+  async post(url: string, data: any) {
+    const res = await fetch(`/api/${url}`, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return await res.json();
+  }
+}

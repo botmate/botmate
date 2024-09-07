a71345151bd35f8e714bf5f418c53ff6e1a2f356
mxvsh <hi@monawwar.io>
2024-09-07 13:28:30 +0530

    chore: add tagTypes for Plugins and BotPlugins in api.ts

--- packages/client/src/application/api.ts
+++ packages/client/src/application/api.ts
@@ -3,6 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const baseApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: '/api' }),
   endpoints: () => ({}),
+  tagTypes: ['Plugins', 'BotPlugins'],
 });
 
 export class Api {

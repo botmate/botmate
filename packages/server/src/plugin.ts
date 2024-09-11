e020fd0ea5815808b87fc9df32ca20e443c2a898
mxvsh <hi@monawwar.io>
2024-09-11 23:46:32 +0530

    refactor: remove unused code and improve error handling in ConfigManager

--- packages/server/src/plugin.ts
+++ packages/server/src/plugin.ts
@@ -3,6 +3,7 @@ import { PlatformType } from '@botmate/platform';
 
 import { Application } from './application';
 import { Bot } from './bot';
+import { IPlugin } from './models/plugin';
 
 export interface PluginInterface {
   beforeLoad?: () => void;
@@ -23,19 +24,24 @@ export abstract class Plugin implements PluginInterface {
   constructor(
     private _app: Application,
     private _bot: Bot,
+    private _data: IPlugin,
   ) {}
 
   get bot() {
     return this._bot;
   }
 
+  get data() {
+    return this._data;
+  }
+
   get config() {
     const configManager = this._app.configManager;
     return {
       get: (key: string, def?: any) =>
-        configManager.getPluginConfig(+this.bot.data.id, key, def),
+        configManager.getPluginConfig(this.data.id, key, def),
       set: (key: string, value: any) =>
-        configManager.savePluginConfig(+this.bot.data.id, key, value),
+        configManager.savePluginConfig(this.data.id, key, value),
     };
   }
 }

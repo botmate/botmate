67b3f0cc7738f8624ca66b9252301f9701f79f4d
mxvsh <hi@monawwar.io>
2024-09-11 11:29:22 +0530

    chore(client): add application hooks and export toast from sonner

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -2,3 +2,10 @@ export * from './application';
 export * from './plugin';
 
 export * from './application/layouts/page';
+
+export * from './application/hooks/use-app';
+export * from './application/hooks/use-bot';
+export * from './application/hooks/use-plugins';
+export * from './application/hooks/use-config';
+
+export { toast } from 'sonner';

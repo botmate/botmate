ae7bba20570865c240bc1430a727087fda2376ca
mxvsh <hi@monawwar.io>
2024-09-12 11:42:05 +0530

    refactor: register dev command in CLI

--- packages/server/src/commands/index.ts
+++ packages/server/src/commands/index.ts
@@ -1,8 +1,10 @@
 import { Application } from '../application';
+import dev from './dev';
 import pm from './pm';
 import start from './start';
 
 export function registerCLI(app: Application) {
   start(app);
+  dev(app);
   pm(app);
 }

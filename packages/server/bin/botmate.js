d41b5a1214128a4c74800e94bde7c0834fc8081e
mxvsh <hi@monawwar.io>
2024-09-12 11:42:47 +0530

    refactor: add botmate.js CLI script and update package.json

---
+++ packages/server/bin/botmate.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+const { spawn } = require('child_process');
+const { existsSync } = require('fs');
+const { dirname, join } = require('path');
+
+process.env['VITE_CJS_IGNORE_WARNING'] = 'true';
+
+const tsConfigPath = join(__dirname, '..', 'tsconfig.json');
+const isTsProject = existsSync(tsConfigPath);
+
+if (isTsProject) {
+  const tsx = join(dirname(require.resolve('tsx')), 'cli.mjs');
+  const cliPath = join(__dirname, '..', 'src', 'cli.ts');
+  const [, , ...argv] = process.argv;
+
+  spawn(
+    tsx,
+    [
+      'watch',
+      '--ignore=./storage/**',
+      '-r',
+      'tsconfig-paths/register',
+      cliPath,
+      ...argv,
+    ],
+    {
+      stdio: 'inherit',
+    },
+  );
+} else {
+  const cliPath = join(__dirname, '..', 'lib', 'cli.js');
+  const [, , ...argv] = process.argv;
+
+  spawn('node', [cliPath, ...argv], {
+    stdio: 'inherit',
+  });
+}

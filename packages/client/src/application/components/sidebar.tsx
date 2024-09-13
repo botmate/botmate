3616195705dd20008a33eeead50ef6fac6721b0d
mxvsh <hi@monawwar.io>
2024-09-14 01:49:24 +0530

    refactor: optimize sidebar rendering logic

--- packages/client/src/application/components/sidebar.tsx
+++ packages/client/src/application/components/sidebar.tsx
@@ -14,44 +14,82 @@ import {
   TooltipTrigger,
 } from '@botmate/ui';
 
+import { useApp } from '../hooks/use-app';
 import useCurrentBot from '../hooks/use-bot';
+import { SidebarItem } from '../application';
 
-const items = [
+const items: SidebarItem[] = [
   {
-    id: 1,
-    title: 'Dashboard',
+    label: 'Dashboard',
     icon: LayoutDashboard,
     path: '/',
     regex: /^\/$/,
   },
   {
-    id: 2,
-    title: 'Analytics',
+    label: 'Analytics',
     icon: ChartAreaIcon,
     path: '/analytics',
     regex: /^\/analytics$/,
   },
   {
-    id: 3,
-    title: 'Marketplace',
+    label: 'Marketplace',
     icon: ShoppingBagIcon,
     path: '/marketplace',
     regex: /^\/marketplace$/,
   },
   {
-    id: 4,
-    title: 'Settings',
+    label: 'Settings',
     icon: Settings2Icon,
     path: '/settings',
     regex: /^\/settings/,
   },
 ];
 
+function SidebarItem({
+  path,
+  isActive,
+  Icon,
+  item,
+}: {
+  path: string;
+  isActive: boolean;
+  Icon: any;
+  item: (typeof items)[0];
+}) {
+  return (
+    <Tooltip delayDuration={0}>
+      <Link
+        to={path}
+        className="flex items-center justify-center cursor-default"
+        draggable="false"
+      >
+        <TooltipTrigger
+          className={
+            `p-3 rounded-xl cursor-default ${isActive ? 'bg-primary/10 text-primary' : 'hover:bg-primary/10 text-black/70 dark:text-white'}` +
+            `transition-all duration-150`
+          }
+        >
+          <Icon
+            size={24}
+            className={
+              isActive ? 'text-primary' : 'text-black/70 dark:text-white/70'
+            }
+          />
+        </TooltipTrigger>
+      </Link>
+      <TooltipContent side="right" sideOffset={10}>
+        <p>{item.label}</p>
+      </TooltipContent>
+    </Tooltip>
+  );
+}
+
 function Sidebar() {
+  const app = useApp();
   const params = useParams();
   const location = useLocation();
   const bot = useCurrentBot();
-
+  
   return (
     <div className="w-24 h-full flex flex-col py-3 bg-card overflow-hidden">
       <div className="w-full h-20 flex items-center justify-center">
@@ -66,44 +104,29 @@ function Sidebar() {
       </div>
 
       <div className="flex flex-col py-4 gap-1 h-full overflow-auto">
-        {items.map((item) => {
-          const Icon = item.icon;
-          const relativePath = location.pathname.replace(/^\/bots\/\d+/, '');
-          const isActive = item.regex.test(relativePath || '/');
+        <TooltipProvider>
+          {[...items,...(app.sidebar.length > 0 ? ['', ...app.sidebar] : [])].map((item, index) => {
+            if (typeof item === 'string') {
+              return <div key={index} className="h-px bg-muted/80 mx-5 my-2" />;
+            }
 
-          const absolutePath = `/bots/${params.id}${item.path}`;
+            const Icon = item.icon;
+            const relativePath = location.pathname.replace(/^\/bots\/\d+/, '');
+            const isActive = item.regex ? item.regex.test(relativePath || '/') : item.path === relativePath;
 
-          return (
-            <TooltipProvider key={item.id}>
-              <Tooltip delayDuration={0}>
-                <Link
-                  to={absolutePath}
-                  className="flex items-center justify-center cursor-default"
-                  draggable="false"
-                >
-                  <TooltipTrigger
-                    className={
-                      `p-3 rounded-xl cursor-default ${isActive ? 'bg-primary/10 text-primary' : 'hover:bg-primary/10 text-black/70 dark:text-white'}` +
-                      `transition-all duration-150`
-                    }
-                  >
-                    <Icon
-                      size={24}
-                      className={
-                        isActive
-                          ? 'text-primary'
-                          : 'text-black/70 dark:text-white/70'
-                      }
-                    />
-                  </TooltipTrigger>
-                </Link>
-                <TooltipContent side="right" sideOffset={10}>
-                  <p>{item.title}</p>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-          );
-        })}
+            const absolutePath = `/bots/${params.botId}${item.path}`;
+
+            return (
+              <SidebarItem
+                key={index}
+                path={absolutePath}
+                isActive={isActive}
+                Icon={Icon}
+                item={item}
+              />
+            );
+          })}
+        </TooltipProvider>
       </div>
       <div className="relative flex items-center justify-center" role="group">
         <img

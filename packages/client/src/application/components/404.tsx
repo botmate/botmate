c55bcadf3d6f66b007eea70ffa6779ae59d79495
mxvsh <hi@monawwar.io>
2024-09-14 01:48:31 +0530

    chore: add 404 page component

---
+++ packages/client/src/application/components/404.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function PageNotFound() {
+  return (
+    <div className="flex-1">
+      <div className="flex items-center justify-center h-full">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold">404</h1>
+          <p className="text-lg">Page not found</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PageNotFound;

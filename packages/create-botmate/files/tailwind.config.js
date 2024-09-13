e82138b8c2f2ac91ca47c8bb198fe9f6ef8d2bd1
mxvsh <hi@monawwar.io>
2024-09-13 18:49:55 +0530

    refactor: update tailwind css content configuration

--- packages/create-botmate/files/tailwind.config.js
+++ packages/create-botmate/files/tailwind.config.js
@@ -3,7 +3,7 @@ const theme = require('@botmate/ui/tailwind.theme.config');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ['class'],
-  content: ['./packages/**/*.{js,jsx,ts,tsx}'],
+  content: ['./packages/*/src/client/**/*.tsx'],
   corePlugins: { preflight: false },
   theme: theme,
   plugins: [require('tailwindcss-animate')],

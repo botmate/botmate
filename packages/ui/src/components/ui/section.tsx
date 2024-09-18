9a73ad550773a0615dfd225a73a68d70bd90f933
mxvsh <hi@monawwar.io>
2024-09-18 17:14:20 +0530

    refactor(ui): add ref prop to Section component

--- packages/ui/src/components/ui/section.tsx
+++ packages/ui/src/components/ui/section.tsx
@@ -9,9 +9,9 @@ type SectionProps = React.InputHTMLAttributes<HTMLInputElement> & {
   className?: string;
 };
 export const Section = React.forwardRef<HTMLDivElement, SectionProps>(
-  ({ title, description, children, className }) => {
+  ({ title, description, children, className }, ref) => {
     return (
-      <div className={cn(className, 'space-y-4')}>
+      <div className={cn(className, 'space-y-4')} ref={ref}>
         <div>
           <h2 className="text-lg font-medium">{title}</h2>
           {description && (

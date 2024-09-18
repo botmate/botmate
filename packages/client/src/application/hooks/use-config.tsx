acddfe4e815124b83b7b6c9d6812e68988b1f8e1
mxvsh <hi@monawwar.io>
2024-09-18 16:11:39 +0530

    refactor(hooks): update usePluginConfig hook to support generic types

--- packages/client/src/application/hooks/use-config.tsx
+++ packages/client/src/application/hooks/use-config.tsx
@@ -1,23 +1,33 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { useState } from 'react';
+
 import { useSavePluginConfigMutation } from '../services';
 import { useCurrentPlugin } from './use-plugins';
 
-export function usePluginConfig() {
+export function usePluginConfig<
+  T = Record<string, string | number | boolean>,
+>() {
   const plugin = useCurrentPlugin();
   const [saveMutation, { isLoading }] = useSavePluginConfigMutation();
 
-  const config = plugin?.config || {};
+  const [config, setConfig] = useState<T>((plugin?.config || {}) as T);
 
   return {
-    save: (key: string, value: string) =>
-      saveMutation({ pluginId: plugin!.id, key, value }).unwrap(),
+    save: (key: keyof T, value: T[keyof T]) =>
+      saveMutation({
+        pluginId: plugin!.id,
+        key: key as string,
+        value,
+      })
+        .unwrap()
+        .then(() => {
+          const newConfig = { ...config, [key]: value } as T;
+          setConfig(newConfig);
+        }),
     isSaving: isLoading,
-    get: function <T = any>(key: string, def?: T) {
-      const value = (config?.[key] || def) as T;
-      if (typeof value === 'number') {
-        return Number(value);
-      } else {
-        return value;
-      }
+    get: function (key: keyof T, def?: T[keyof T]) {
+      const value = config?.[key] ?? def;
+      return value;
     },
   };
 }

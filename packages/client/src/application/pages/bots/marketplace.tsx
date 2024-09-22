128d5ad9c864c0f792ed8c277002b26d0f016ddb
mxvsh <hi@monawwar.io>
2024-09-23 01:55:06 +0530

    chore: unused import

--- packages/client/src/application/pages/bots/marketplace.tsx
+++ packages/client/src/application/pages/bots/marketplace.tsx
@@ -1,5 +1,5 @@
 import { ImageOff, RefreshCcw, UploadIcon, VerifiedIcon } from 'lucide-react';
-import React, { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import {

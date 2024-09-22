96f21870deb9396d2d9ce5f5a96a3d88eaf6820e
mxvsh <hi@monawwar.io>
2024-09-22 20:06:16 +0530

    chore: add rpc service and trpc router

--- packages/server/src/services/_trpc.ts
+++ packages/server/src/services/_trpc.ts
@@ -3,6 +3,7 @@ import { initTRPC } from '@trpc/server';
 import { Application } from '../application';
 import { BotsService } from './bots.service';
 import { PluginsService } from './plugins.service';
+import { RPCService } from './rpc.service';
 
 const t = initTRPC.create();
 
@@ -12,6 +13,7 @@ export function initTrpc(app: Application) {
   const appRouter = t.router({
     ...new BotsService(app).getRoutes(),
     ...new PluginsService(app).getRoutes(),
+    ...new RPCService(app).getRoutes(),
   });
 
   return appRouter;
